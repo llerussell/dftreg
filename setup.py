3d7b647cd6c1ff1d7383b9cd706362627d7a0624
Lloyd <lrussell@Lloyds-MacBook-Pro.local>
2015-04-06 12:42:28 +0100

    Add new files

---
+++ setup.py
@@ -0,0 +1,7 @@
+from distutils.core import setup
+setup(name='dftreg',
+      version='0.1',
+      description='Image sequence motion correction',
+      author='Lloyd Russell',
+      py_modules=['dftreg'],
+      )
